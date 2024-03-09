77e6783f8bc042c7697c23c0b4fb3cd52fd18761
JoshuaWoods <nordev@protonmail.com>
2024-03-09 00:43:34 -0600

    need to update the server

---
+++ my-project/src/App.js
@@ -0,0 +1,13 @@
+// App.js
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className='main'>
+      <h1>Hello, React!</h1>
+    </div>
+  );
+}
+
+export default App;
--- seed.js
+++ seed.js
@@ -1,25 +1,30 @@
+const { execSync } = require('child_process');
 const fs = require('fs');
 const path = require('path');
 
-//Create project base files
+//Create function to write react, vite and such into package.json
+
 
 function createBaseProjectStructure(projectName) {
 
     fs.mkdirSync(projectName);
 
     //create folders
-    const directories = [ 'public', 'src', 'config'];
+    const directories = ['public', 'src', 'config', 'test'];
 
     directories.forEach(directory => {
         fs.mkdirSync(path.join(projectName, directory));
     });
 
-    //Starter files for the folders
+    // Starter files for the folders
     fs.writeFileSync(path.join(projectName, 'public', 'index.html'), '<!DOCTYPE html>\n<html>\n<head>\n<title>Welcome to Your Project</title>\n</head>\n<body>\n<h1>Welcome to Your Project</h1>\n</body>\n</html>');
 
     fs.writeFileSync(path.join(projectName, 'src', 'index.js'), '// Your main entry point for the project');
 
+
+
     console.log('Base project structure created successfully!');
 }
+
 
 createBaseProjectStructure('my-project');
